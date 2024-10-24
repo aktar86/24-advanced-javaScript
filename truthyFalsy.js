9d6b7db14ff24b115f78b83715529d50ecf6fdc5
aktar86 <amdaktar86@gmail.com>
2024-10-25 00:32:05 +0600

    true vs false

--- truthyFalsy.js
+++ truthyFalsy.js
@@ -1,1 +1,21 @@
-console.log(12)
+// Falsy:
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+// Truthy:
+// '0',
+// WhiteSpace: " ",
+// {}
+// []
+
+const names = "sadiaAyman";
+if(names.length >= 10){
+    console.log("this condition is truth")
+}
+else{
+    console.log("this condition is false")
+}
