f9bea7de35a892a40224c7a1abaed14aaa174690
aktar86 <amdaktar86@gmail.com>
2024-10-25 00:53:42 +0600

    null vs undefined learned

---
+++ nullVsUndefined.js
@@ -0,0 +1,20 @@
+
+function addNumber (n1, n2){
+    console.log(n1 + n2)
+    // return; //undefined
+    return n1 * n2;
+}
+
+let result = addNumber(7, 5);
+console.log(result) 
+
+
+const friendDe = {name:"Toma", in: 1035, phone: '01245', occupesson: "student", bf: "no iching-Piching"}
+console.log(friendDe.bf) //undefined
+
+
+let fun = undefined;
+console.log(fun) //undefined
+
+const ages = [2, 6, 9]
+console.log(ages[11]) //undefined
